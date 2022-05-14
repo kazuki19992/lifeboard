6c825be450b7ef72df431fbf44448529d98b66ca
kazuki19992 <kushida98@gmail.com>
2022-05-14 22:11:02 +0900

    Fix: EEW.tsx Style

--- components/organisms/EEW/EEW.tsx
+++ components/organisms/EEW/EEW.tsx
@@ -12,6 +12,25 @@ interface Props {
 const EEW = (props: Props) => {
   // 緊急地震速報とかを表示するやつだよ
 
+  const getEarthquakeLevel = () => {
+    switch (true) {
+      case props.magnitude <= 4: {
+        return 2;
+      }
+      case props.magnitude <= 6: {
+        return 1;
+      }
+      default: {
+        return 0;
+      }
+    }
+  }
+  const earthquakeLevels = [
+    {ja: "かなり強い", en: "A very big"},
+    {ja: "強い", en: "A big"},
+    {ja: null, en: "An"},
+  ]
+
   return (
     <div className={cx("h-2/5", "bg-orange-600", "text-3xl", "fixed", "bottom-0", "w-screen", "left-0", "right-0", "z-50")}>
       <div className={cx("w-11/12", "text-white", "font-bold", "mx-auto", "py-12", "flex", "justify-around", "items-center")}>
@@ -20,9 +39,9 @@ const EEW = (props: Props) => {
         </div>
         <div>
           <h1 className={cx("text-8xl", "mb-3")}>緊急地震速報! 身を守って!</h1>
-          <h2 className={cx("text-3xl", "my-2", "mb-16")}>Earthquake alert! A big earthquake is coming! Get yourself to a safe place!</h2>
-          <h2 className={cx("text-5xl", "my-2")}>{props.place.ja}でかなり強い地震 推定震度<span className="text-7xl">4</span> マグニチュード<span className="text-7xl">{props.magnitude}</span></h2>
-          <h2 className={cx("text-3xl", "my-2")}>Earthquake off the coast of {props.place.en}. Estimated seismic intensity 4   magnitude {props.magnitude}</h2>
+          <h2 className={cx("text-3xl", "my-2", "mb-16", "font-normal")}>Earthquake alert! {earthquakeLevels[getEarthquakeLevel()].en} earthquake is coming! Get yourself to a safe place!</h2>
+          <h2 className={cx("text-5xl", "my-2")}>{props.place.ja}で{earthquakeLevels[getEarthquakeLevel()].ja}地震 推定震度<span className="text-7xl">4</span> マグニチュード<span className="text-7xl">{props.magnitude}</span></h2>
+          <h2 className={cx("text-3xl", "my-2", "font-normal")}>Earthquake off the coast of {props.place.en}. Estimated seismic intensity 4   magnitude {props.magnitude}</h2>
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import type { NextPage } from 'next'
 import { cx, css } from '@emotion/css'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import EEW from '../components/organisms/EEW/EEW'
 
 const Home: NextPage = () => {
   return (
     <>
-    <div className={cx("w-11/12", "ml-auto", "mr-auto")}>
+    <div className={cx("w-11/12", "mx-auto")}>
+      <div className={cx("flex", "items-center", "space-x-1", "justify-between")}>
+
+      </div>
     </div>
-    <EEW place={{ja: "福島沖", en: "Fukushima"}} magnitude={7} />
+    <div className={cx()}>
+      <EEW place={{ja: "${震源}", en: "${PlaceName}"}} magnitude={8} />
+    </div>
     </>
   )
 }
